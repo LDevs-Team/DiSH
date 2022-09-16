04f6ed43c76d8fd88a7a0baf9d2eb25b583a2283
StarNumber <64470722+StarNumber12046@users.noreply.github.com>
2022-09-16 16:00:26 +0200

    Update build.py

--- build.py
+++ build.py
@@ -8,10 +8,9 @@ exitcode = os.system("pyinstaller --name DiSH --onefile --windowed main.py")
 print("Moving files")
 
 g = glob.glob("dist/DiSH*")
-
+print(g)
 print("Creating zip")
 with zipfile.ZipFile("dist/DiSHv{}-{}.zip".format(ver, platform.system()), "w", zipfile.ZIP_DEFLATED) as zip:
-    zip.write()
     zip.write("main.py")
     zip.write("setup-registry.bat")
     zip.write("setup-shell-startup.bat")
