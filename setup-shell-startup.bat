4c9ece817e187fdbd7559d314ecadc766feafeb3
XPower7125 <64551044+XPower7125@users.noreply.github.com>
2023-07-07 13:17:02 +0200

    Delete setup-shell-startup.bat

--- setup-shell-startup.bat
+++
@@ -1,5 +0,0 @@
-pip install -r requirements.txt
-mkdir %appdata%\LDevs\DiSH
-copy DiSH.exe %appdata%\LDevs\DiSH
-copy main.py %appdata%\LDevs\DiSH
-mklink "%appdata%\Microsoft\Windows\Start Menu\Programs\Startup\DiSH.exe" %appdata%\DiSH\DiSH.exe
