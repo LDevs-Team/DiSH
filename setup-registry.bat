20d404cf77d3b68b0d6c0faa6ed5729c0b21429d
XPower7125 <64551044+XPower7125@users.noreply.github.com>
2023-07-07 13:03:11 +0200

    feat: unified with DiSHLoader

--- setup-registry.bat
+++ setup-registry.bat
@@ -1,7 +1,7 @@
-mkdir %appdata%\DiSH
-copy DiSH.exe %appdata%\DiSH
-copy .env %appdata%\DiSH
-copy cz.yaml %appdata%\DiSH
+mkdir %appdata%\LDevs\DiSH
+copy DiSH.exe %appdata%\LDevs\DiSH
+copy .env %appdata%\LDevs\DiSH
+copy cz.yaml %appdata%\LDevs\DiSH
 reg add HKCU\Software\Microsoft\Windows\CurrentVersion\Run\ /V "DiSH" /t REG_SZ /F /D "%appdata%\DiSH\DiSH.exe"
 echo starting...
-start %appdata%\DiSH\DiSH.exe
+start %appdata%\LDevs\DiSH\DiSH.exe
