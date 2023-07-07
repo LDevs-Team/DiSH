d1c9e76d00a698480f9ebe5ae077b5c776c62ad3
XPower7125 <64551044+XPower7125@users.noreply.github.com>
2023-07-07 13:17:33 +0200

    Delete setup-registry.bat

--- setup-registry.bat
+++
@@ -1,7 +0,0 @@
-mkdir %appdata%\LDevs\DiSH
-copy DiSH.exe %appdata%\LDevs\DiSH
-copy .env %appdata%\LDevs\DiSH
-copy cz.yaml %appdata%\LDevs\DiSH
-reg add HKCU\Software\Microsoft\Windows\CurrentVersion\Run\ /V "DiSH" /t REG_SZ /F /D "%appdata%\DiSH\DiSH.exe"
-echo starting...
-start %appdata%\LDevs\DiSH\DiSH.exe
