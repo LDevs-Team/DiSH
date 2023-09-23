30596a26e7b3a282a52eb09cadd3b05b75d95420
StarNumber <64470722+StarNumber12046@users.noreply.github.com>
2023-09-23 11:56:46 +0200

    fix: forced reg add in setup !

--- setup.bat
+++ setup.bat
@@ -1,3 +1,3 @@
 mkdir %appdata%\ldevs\dishloader\versions\nightly
 copy * %appdata%\ldevs\dishloader\versions\nightly
-reg add HKCU\SOFTWARE\LDevs\DiSHLoader /v DefaultVersion /t REG_SZ /d nightly
+reg add HKCU\SOFTWARE\LDevs\DiSHLoader /v DefaultVersion /t REG_SZ /d nightly /f
